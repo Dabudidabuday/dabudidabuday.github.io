a3f76008931657333047903bb738718e94fb0628
Myroslav <94mir.ms@gmail.com>
2020-03-24 23:06:46 +0200

    update

---
+++ app/js/main.js
@@ -0,0 +1,84 @@
+
+$(document).ready(function () {
+    $('#product__slider-1').slick({
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+    });
+
+    $('#product__slider-2').slick({
+        prevArrow: $('.prev--2'),
+        nextArrow: $('.next--2'),
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+
+    });
+});
+
+
+// 'use strict';
+
+// OVERLAY
+let overlay = document.querySelector('#overlay');
+
+// MODAL WINDOWS
+let modalPartners = document.querySelector('.modal__partners');
+let modalConsultation = document.querySelector('.modal__consultation');
+
+// MODAL OPEN BUTTONS
+let introBtn = document.querySelector('.btn--intro');
+
+
+let modalCloseBtn = document.querySelector('.modal__close');
+
+
+
+
+
+function modalOpen(button, form) {
+    button.addEventListener('click', function () {
+        overlay.classList.remove('undisplay');
+        form.classList.remove('undisplay');
+    })
+};
+
+function modalClose(form) {
+    modalCloseBtn.addEventListener('click', function () {
+        overlay.classList.add('undisplay');
+        form.classList.add('undisplay');
+    });
+
+    window.onclick = function (event) {
+        if (event.target == overlay) {
+            overlay.classList.add("undisplay");
+            form.classList.add("undisplay");
+        }
+    };
+};
+
+modalOpen(introBtn, modalPartners);
+modalClose(modalPartners);
+
+/* 
+** Header menu
+*/
+
+let toggleMenuBtn = document.querySelector('.header__burger'),
+    headerNavMenu = document.querySelector('.header__nav');
+
+toggleMenuBtn.addEventListener('click', function () {
+    headerNavMenu.classList.toggle('show');
+    toggleMenuBtn.classList.toggle('show');
+})
+
+
+
+
+
+
+
