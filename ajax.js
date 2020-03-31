c7b0eb1bfe1bbc241f9d0048dcd7cc8654c53240
Myroslav <Myroslav.sirenko@tateeda.com>
2020-03-31 21:34:51 +0300

    add ajax files

---
+++ ajax.js
@@ -0,0 +1,24 @@
+$( document ).ready(function() {
+    $("#feedbackBtn").click(
+		function(){
+			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+			return false; 
+		}
+	);
+});
+ 
+function sendAjaxForm(result_form, ajax_form, url) {
+    jQuery.ajax({
+        url:     url, //url страницы (action_ajax_form.php)
+        type:     "POST", //метод отправки
+        dataType: "html", //формат данных
+        data: jQuery("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+        	result = jQuery.parseJSON(response);
+        	document.getElementById(result_form).innerHTML = "Имя: "+result.name+"<br>Телефон: "+result.phonenumber;
+    	},
+    	error: function(response) { // Данные не отправлены
+    		document.getElementById(result_form).innerHTML = "Ошибка. Данные не отправленны.";
+    	}
+ 	});
+}
